89d18793133d503fb2aa02844a61f061325f1fc6
Lucca Carlini <lucca.carlini18@gmail.com>
2020-07-22 04:06:45 -0300

    Testando classe de domínio Categoria

--- src/main/java/com/mouzetech/maniadecrepeapp/resources/CategoriaResource.java
+++ src/main/java/com/mouzetech/maniadecrepeapp/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.mouzetech.maniadecrepeapp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mouzetech.maniadecrepeapp.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Jardinagem");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 	
 }
