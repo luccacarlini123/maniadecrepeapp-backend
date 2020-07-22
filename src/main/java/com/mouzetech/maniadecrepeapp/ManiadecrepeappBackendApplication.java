f030bea46c9786c227990e16e054dfd79a1b5da9
Lucca Carlini <lucca.carlini18@gmail.com>
2020-07-22 03:51:48 -0300

    Projeto criado

---
+++ src/main/java/com/mouzetech/maniadecrepeapp/ManiadecrepeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mouzetech.maniadecrepeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiadecrepeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManiadecrepeappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouzetech/maniadecrepeapp/ManiadecrepeappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouzetech.maniadecrepeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiadecrepeappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
