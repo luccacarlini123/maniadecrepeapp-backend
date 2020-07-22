62f667e835c1f96346f599195b18ab98b83d3c2d
Lucca Carlini <lucca.carlini18@gmail.com>
2020-07-22 16:02:50 -0300

    Ajustando endpoint /categorias/{id}

--- src/main/java/com/mouzetech/maniadecrepeapp/services/CategoriaService.java
+++ src/main/java/com/mouzetech/maniadecrepeapp/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.mouzetech.maniadecrepeapp.domain.Categoria;
 import com.mouzetech.maniadecrepeapp.repositories.CategoriaRepository;
+import com.mouzetech.maniadecrepeapp.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscarPorId(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id "+id+" não encontrado. Tipo: "+Categoria.class+""));
 	}
 	
 }
