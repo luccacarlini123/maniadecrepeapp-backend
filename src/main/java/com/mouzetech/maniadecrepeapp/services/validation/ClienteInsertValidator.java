36cb1e2132d989c5bfa0c6412fccc66b28be4d97
Lucca Carlini <lucca.carlini18@gmail.com>
2020-08-02 03:24:48 -0300

    Validação customizada Email já existente

--- src/main/java/com/mouzetech/maniadecrepeapp/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/mouzetech/maniadecrepeapp/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mouzetech.maniadecrepeapp.domain.Cliente;
 import com.mouzetech.maniadecrepeapp.domain.enums.TipoCliente;
 import com.mouzetech.maniadecrepeapp.dto.ClienteNewDTO;
+import com.mouzetech.maniadecrepeapp.repositories.ClienteRepository;
 import com.mouzetech.maniadecrepeapp.resources.exceptions.FieldMessage;
 import com.mouzetech.maniadecrepeapp.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux!=null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
