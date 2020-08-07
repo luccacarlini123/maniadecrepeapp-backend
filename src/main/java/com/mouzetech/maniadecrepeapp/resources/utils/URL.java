2164d6180a48235476bfe673883fa57084a68174
Lucca Carlini <lucca.carlini18@gmail.com>
2020-08-07 03:56:49 -0300

    Busca de produto por nome e categorias

---
+++ src/main/java/com/mouzetech/maniadecrepeapp/resources/utils/URL.java
@@ -0,0 +1,29 @@
+package com.mouzetech.maniadecrepeapp.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+
+	public static List<Integer> decodeIntList(String s){
+		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+		for(int i=0; i<vet.length; i++) {
+			list.add(Integer.valueOf(vet[i]));
+		}
+		return list;
+		//abaixo usando o lambida com apenas uma linha conseguimos fazer todo o código digitado acima
+		//return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+	
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+}
---
+++ src/main/java/com/mouzetech/maniadecrepeapp/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.mouzetech.maniadecrepeapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.mouzetech.maniadecrepeapp.domain.Categoria;
+import com.mouzetech.maniadecrepeapp.domain.Produto;
+import com.mouzetech.maniadecrepeapp.repositories.CategoriaRepository;
+import com.mouzetech.maniadecrepeapp.repositories.ProdutoRepository;
+import com.mouzetech.maniadecrepeapp.services.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+
+	@Autowired
+	private ProdutoRepository produtoRepo;
+	
+	public Produto buscarPorId(Integer id) {
+		Optional<Produto> obj = produtoRepo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Produto não encontrado! Id: "+id+", Tipo: "+Produto.class));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepo.findAllById(ids);
+		return produtoRepo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+	
+}
