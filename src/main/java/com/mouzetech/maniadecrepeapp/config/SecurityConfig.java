620a888062f1effbf4d658a8acb86b9ae6193a39
Lucca Carlini <lucca.carlini18@gmail.com>
2020-08-14 22:17:47 -0300

    Implementando autenticação, geração do token JWT e autorização

--- src/main/java/com/mouzetech/maniadecrepeapp/config/SecurityConfig.java
+++ src/main/java/com/mouzetech/maniadecrepeapp/config/SecurityConfig.java
@@ -2,26 +2,38 @@ package com.mouzetech.maniadecrepeapp.config;
 
 import java.util.Arrays;
 
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.mouzetech.maniadecrepeapp.security.JWTAuthenticationFilter;
+import com.mouzetech.maniadecrepeapp.security.JWTAuthorizationFilter;
+import com.mouzetech.maniadecrepeapp.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private Environment env;
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+    private Environment env;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 		"/h2-console/**"
@@ -43,8 +55,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+	
+	@Override
+	 public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	 }
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
---
+++ src/main/java/com/mouzetech/maniadecrepeapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.mouzetech.maniadecrepeapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mouzetech.maniadecrepeapp.domain.Cliente;
+import com.mouzetech.maniadecrepeapp.repositories.ClienteRepository;
+import com.mouzetech.maniadecrepeapp.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
